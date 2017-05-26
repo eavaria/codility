3dcdb7f80a0b71dac941f73c7a68b26a4445f018
GLOBANT\eduardo.avaria <eduardo.avaria@globant.com>
2017-05-26 18:24:52 -0400

    7a

--- lesson_7a.py
+++ lesson_7a.py
@@ -11,9 +11,14 @@ def solution(S):
         if s in openings:
             L.append(s)
         elif s in closings:
-            if d[s] != L.pop():
-                return 0                
-    return 1
+            if len(L) == 0 or d[s] != L.pop():
+                return 0
+        else:
+            return 0
+    if len(L) != 0:
+        return 0
+    else:
+        return 1
     
 
 
@@ -21,6 +26,7 @@ S=[]
 S.append([["{[()()]}"],1])
 S.append([["([)()]"],0])
 S.append([[""],1])
+S.append([["{{{{"],0])
 
 
 
